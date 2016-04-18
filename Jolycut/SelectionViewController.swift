25e1d60efd3391c4ecea6c279ad4193fb3ddfe24
Vinod AROUMOUGAME <aroumo_v@g03-r1p07.local>
2016-04-18 11:07:27 +0200

    test2.0

---
+++ Jolycut/SelectionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SelectionViewController.swift
+//  JoliCut
+//
+//  Created by Quentin LE GAL on 17/04/16.
+//  Copyright © 2016 Vinod AROUMOUGAME. All rights reserved.
+//
+
+import UIKit
+
+class SelectionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
