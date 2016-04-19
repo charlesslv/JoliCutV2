4aaecc7a63e654299068469e19ca32a42e2d85c1
Quentin LE GAL <legal_q@g03-r2p06.local>
2016-04-19 11:58:55 +0200

    We can now add user from the inscription sheet, and go back to login screen (still have to do the regex so we won't have any issue about wrong formatted user data)..

--- Jolycut/InscriptionViewController.swift
+++ Jolycut/InscriptionViewController.swift
@@ -11,6 +11,14 @@ import UIKit
 
 class InscriptionViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet var Email: UITextField!
+    @IBOutlet var Password: UITextField!
+    @IBOutlet var LastName: UITextField!
+    @IBOutlet var FirstName: UITextField!
+    
+    @IBAction func Signup(sender: AnyObject) {
+    }
+    
     func CloseKeyboard() {
         view.endEditing(true)
     }
@@ -28,9 +36,88 @@ class InscriptionViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        
         textField.resignFirstResponder()
-        
         return true
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        let whitespace = NSCharacterSet.whitespaceCharacterSet()
+        let range = Email.text!.rangeOfCharacterFromSet(whitespace)
+        let range0 = Password.text!.rangeOfCharacterFromSet(whitespace)
+        let range1 = LastName.text!.rangeOfCharacterFromSet(whitespace)
+        let range2 = FirstName.text!.rangeOfCharacterFromSet(whitespace)
+        if (Email.text == ("") || Password.text == (""))
+        {
+            let myAlert = UIAlertController(title: "Error", message: "Can't let empty field !", preferredStyle: UIAlertControllerStyle.Alert)
+            let dismiss = UIAlertAction(title: "Return", style: UIAlertActionStyle.Default){(ACTION) in print("Dismissed");}
+            myAlert.addAction(dismiss)
+            self.presentViewController(myAlert, animated: true, completion: nil)
+            return false
+        }
+        else if ((range != nil) && (range0 != nil) && (range1 != nil) && (range2 != nil)) {
+            let myAlert = UIAlertController(title: "Error", message: "Login can't contain whitespace !", preferredStyle: UIAlertControllerStyle.Alert)
+            let dismiss = UIAlertAction(title: "Return", style: UIAlertActionStyle.Default){(ACTION) in print("spacefound");}
+            myAlert.addAction(dismiss)
+            self.presentViewController(myAlert, animated: true, completion: nil)
+            return false
+        }
+        else
+        {
+            let url:NSURL = NSURL(string: "http://92.222.74.85/api/signup/"+LastName.text!+"/"+FirstName.text!+"/"+Password.text!+"/"+Email.text!)!
+            let session = NSURLSession.sharedSession()
+            let request = NSMutableURLRequest(URL: url)
+            request.HTTPMethod = "POST"
+            request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringCacheData
+            let data = "data=Hi".dataUsingEncoding(NSUTF8StringEncoding)
+            let task = session.uploadTaskWithRequest(request, fromData: data, completionHandler:
+                {(data,response,error) in
+                    if (nil != response as? NSHTTPURLResponse) {
+                        let httpResponse = response as? NSHTTPURLResponse
+                        MyVariables.ErrorCode = httpResponse!.statusCode
+                        if (httpResponse!.statusCode != 404) {
+                            let urlContents = try! NSString(contentsOfURL: url, encoding: NSUTF8StringEncoding)
+                            guard let _:NSString = urlContents else {
+                                print("error")
+                                return
+                            }
+                            MyVariables.data = urlContents
+                            print(MyVariables.data)
+                        }
+                    }
+                    else {
+                        MyVariables.ErrorCode = 8
+                    }
+                    guard let _:NSData = data, let _:NSURLResponse = response  where error == nil else {
+                        print("error")
+                        return
+                    }
+                    let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                    print(dataString)
+                }
+            )
+            while (MyVariables.ErrorCode != 404 || MyVariables.ErrorCode != 8)
+            {
+                task.resume()
+                print(MyVariables.data)
+                if (MyVariables.ErrorCode == 404)
+                {
+                    MyVariables.ErrorCode = 0
+                    let myAlert = UIAlertController(title: "SUCCESS !", message: "Your inscription is Finished", preferredStyle: UIAlertControllerStyle.Alert)
+                    myAlert.addAction(UIAlertAction(title:"Login Now !", style: .Default, handler:  {action in self.performSegueWithIdentifier("Connect", sender: self)}))
+                    self.presentViewController(myAlert, animated: true, completion: nil)
+                    return true
+                }
+                else if (MyVariables.ErrorCode == 8)
+                {
+                    MyVariables.ErrorCode = 0
+                    let myAlert = UIAlertController(title: "Error", message: "Can't connect to server", preferredStyle: UIAlertControllerStyle.Alert)
+                    let dismiss = UIAlertAction(title: "Return", style: UIAlertActionStyle.Default){(ACTION) in print("Server can't respond !");}
+                    myAlert.addAction(dismiss)
+                    self.presentViewController(myAlert, animated: true, completion: nil)
+                    return false
+                }
+            }
+        }
+        return false
     }
 }
